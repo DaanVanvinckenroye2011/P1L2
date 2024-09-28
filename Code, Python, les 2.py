5fa70b8d5f666062c062e86e33ae6cf31fe1ce0f
DaanVanvinckenroye2011 <vanvinckenroye.daan2011@gmail.com>
2024-09-28 10:29:23 +0200

    Add files via upload

---
+++ Code, Python, les 2.py
@@ -0,0 +1,6 @@
+def naam_en_leeftijd():
+2	    naam = input("Wat is jouw naam?")
+3	    leeftijd = input("Wat is jouw leeftijd?")
+4	    print("Jouw naam is", naam, "en jouw leeftijd is", leeftijd)
+5	    leeftijd_binnen_jaar = leeftijd + 1
+6	    print(leeftijd_binnen_jaar)

26a8719308b74f2176e8727127120e45ea0848b2
DaanVanvinckenroye2011 <vanvinckenroye.daan2011@gmail.com>
2024-09-28 10:44:59 +0200

    Update Code, Python, les 2.py

--- Code, Python, les 2.py
+++ Code, Python, les 2.py
@@ -1,6 +1,15 @@
 def naam_en_leeftijd():
-2	    naam = input("Wat is jouw naam?")
-3	    leeftijd = input("Wat is jouw leeftijd?")
-4	    print("Jouw naam is", naam, "en jouw leeftijd is", leeftijd)
-5	    leeftijd_binnen_jaar = leeftijd + 1
-6	    print(leeftijd_binnen_jaar)
+	    naam = input("Wat is jouw naam?")
+	    leeftijd = input("Wat is jouw leeftijd?")
+	    print("Jouw naam is", naam, "en jouw leeftijd is", leeftijd)
+	    leeftijd_binnen_jaar = leeftijd + 1
+	    print(leeftijd_binnen_jaar)
+
+
+def naam_leeftijd_plaats_schooljaar():
+    naam = input("Geef jouw naam")
+    leeftijd = input("Geef je leeftijd")
+    plaats = input("Geef je favoriete plaats")
+    schooljaar = input("Welk school jaar zit je")
+    
+    story  = print("Er was eens een kind met de naam", naam, "Hij was", leeftijd, "jaar en zat in het", schooljaar, ".", "Zijn lievelings plaats was", plaats, "."
